c004bbeaf4ce83a45f2f8cb67b2a0d18f4770ca0
Derrek Leute <dleute@allofzero.com>
2014-11-12 10:16:40 -0500

    deploy changes

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,8 +3,10 @@ set :domain,      "chatdeploy.allofzero.com"
 set :deploy_to,   "/Users/dleute/Documents/IBM/chatdeploy"
 set :app_path,    "app"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
-set :scm,         :git
+set   :scm,           :git
+set   :repository,    "file:///Users/dleute/Documents/IBM/chatserver"
+set :deploy_via, :rsync_with_remote_cache
+#set   :deploy_via,    :copy
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
 
 set :model_manager, "doctrine"
@@ -13,6 +15,7 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
+set   :use_sudo,      false
 set  :keep_releases,  3
 
 # Be more verbose by uncommenting the following line
